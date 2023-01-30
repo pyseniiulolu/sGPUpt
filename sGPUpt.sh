f11931ef09423bb20fa06461f0de0279bbafd94e
aclist <null>
2023-01-31 03:47:41 +0900

    fix: spaces around disk_pretty

--- sGPUpt.sh
+++ sGPUpt.sh
@@ -766,9 +766,9 @@ function CreateVM()
     read -p "$(logger info "Do you want to create a drive named ${VMName}${DEFAULT}? [y/N]: ")" CHOICE
     case $CHOICE in
 	    y|Y) HandleDisk 
-		 disk_pretty="Disk: \"$DiskSize\","
+		 disk_pretty=" Disk: \"$DiskSize\", "
 		 ;;
-	    ""|N) disk_pretty=""
+	    ""|N) disk_pretty=" "
     esac
     read -p "$(logger info "Do you want to overwrite a drive named ${VMName}? [y/N]: ")" CHOICE
     if [[ $CHOICE == @("y"|"Y") ]]; then
@@ -786,7 +786,7 @@ function CreateVM()
   Emulator="/etc/sGPUpt/qemu-system-x86_64"
   cp $edkDir/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_VARS.fd $OVMF_VARS
 
-  logger info "Creating VM [ Type:\"$SysType\", Name:\"$VMName\", vCPU:\"$vCPU\", Mem:\"$vMem"\M"\", $disk_pretty QEMU-V:\"$vQEMU\" ]"
+  logger info "Creating VM [ Type:\"$SysType\", Name:\"$VMName\", vCPU:\"$vCPU\", Mem:\"$vMem"\M"\",${disk_pretty}QEMU-V:\"$vQEMU\" ]"
 
   virt-install \
   --connect qemu:///system \
