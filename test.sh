ec08ae35ded0091dbf80dc102455c12b2ac862d0
aclist <null>
2023-02-01 05:20:06 +0900

    fix: faster performance

--- test.sh
+++ test.sh
@@ -3,50 +3,48 @@
 function CheckIOMMUGroupsTest()
 {
   ((h=0, allocateGPUOnCycle=0))
-
-  #if [[ $GPUType == "NVIDIA" ]]; then
-    GrepGPU="NVIDIA"
-  #elif [[ $GPUType == "AMD" ]]; then
-   # GrepGPU="AMD/ATI"
-  #fi
-
+GPUType=NVIDIA
+  case $GPUType in
+	  NVIDIA) GrepGPU="NVIDIA" ;;
+	  AMD) GrepGPU="AMD/ATI" ;;
+  esac
   for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
 
     # Check each device in the group to ensure that our target device is isolated properly
     for d in $g/devices/*; do
-      echo -e "\tGroup $(tput setaf 99)${g##*/}$(tput sgr0) - $(lspci -nns ${d##*/})"
-
       deviceID=$(echo ${d##*/} | cut -c6-)
-      deviceOutput=$(lspci -nn -s $deviceID)
+      deviceOutput=$(lspci -nns $deviceID)
+      gr=$(tput setaf 99)${g##*/}$(tput sgr0)
+      echo -e "\tGroup $gr - $deviceOutput"
 
       # If the device isn't part of our GPU then continue checking group
-      if [[ ! $deviceOutput == @(*"VGA"*|*"Audio"*) ]] || [[ ! $deviceOutput =~ "$GrepGPU" ]]; then
-        ((miscDevice+=1))
-        continue
-      fi
-
-      # If there's *currently* no other device in the group then add the device to the array
-      if (( $miscDevice == 0 )); then
-        aGPU[$h]=$deviceID
-        ((h++, allocateGPUOnCycle=1))
-        echo -e "\t+        $(tput setaf 2)✔️$(tput sgr0) ^"
+       if [[ $deviceOutput =~ (VGA|Audio) ]] && [[ $deviceOutput =~ $GrepGPU ]]; then
+         aGPU[$h]=$deviceID
+         ((h++, allocateGPUOnCycle=1))
+         tput cuu1
+         echo -e "      $(tput setaf 2)>$(tput sgr0)"
+       else
+         ((miscDevice++))
       fi
     done
 
     # If $aGPU was defined earlier but it turns out to be in an unisolated group then dump the variable
-    if [[ -n $aGPU ]] && (( $miscDevice > 0 )) && (( $allocateGPUOnCycle == 1 )); then
+    if [[ ${#aGPU[@]} -gt 0 ]] && [[ $miscDevice -gt 0 ]] && [[ $allocateGPUOnCycle -eq 1 ]]; then
       unset aGPU
     fi
-
-    ((miscDevice=0, allocateGPUOnCycle=0))
+    unset miscDevice allocateGPUOnCycle
   done
 
-  if (( ${#aGPU[@]} != 2 )); then
-    echo "GPU is not $(tput setaf 1)invalid$(tput sgr0) for passthrough!"
-    exit 1
-  fi
+  invalid=$(tput setaf 1)invalid$(tput sgr0)
+  valid=$(tput setaf 2)valid$(tput sgr0)
+  case ${#aGPU[@]} in
+    2) echo -e "GPU is $valid for passthrough! = [ ${aGPU[*]} ]" ;;
+    *)
+       echo "GPU is $invalid for passthrough!"
+       exit 1
+       ;;
+  esac
 
-  echo -e "GPU is $(tput setaf 2)valid$(tput sgr0) for passthrough! = [ ${aGPU[@]} ]"
 }
 
 CheckIOMMUGroupsTest
